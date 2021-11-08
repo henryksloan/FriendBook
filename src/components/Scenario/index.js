f6fbefacc7971eab7076fb93b661516963f3328d
Moses Namara <moses@199.250.21.198.tigernet.wifi.dyn.clemson.edu>
2021-11-08 15:55:52 -0500

    Fixed previous comments and format oof json file

--- src/components/Scenario/index.js
+++ src/components/Scenario/index.js
@@ -11,7 +11,7 @@ function Scenario() {
 
       <p className="scenario-text">You are <strong><ProfileLink name="alex_doe"/> </strong> from Fresno. California and regularly use FriendBook ( a social media site) for professional and leisure activities. You are planning on applying for a job, go through the posts you have made in the recent past and see if you are okay with them.</p>
 
-      <p className="scenario-task">For each of your posts, you could consider taking any of the following actions based on how you feel or think the post would reflect on you to a potential employer.</p>
+      <p className="scenario-task">For each of your posts, you should consider taking any of the following actions based on how you feel or think the post would reflect on you to a potential employer.</p>
 
       <ol className="scenario-list">
         <li className="scenario-list-item tooltip">
@@ -20,8 +20,8 @@ function Scenario() {
         </li>
 
         <li className="scenario-list-item tooltip">
-          <label>Change the post’s audeince </label>
-          <span className="tooltiptext"> It’s not uncommon for people to change the audience of their post to control who can view/access their posts.</span>
+          <label>Change the post’s audience </label>
+          <span className="tooltiptext"> It’s not uncommon for people to change the audience of their posts to control who can view/access their posts.</span>
         </li>
 
         <li className="scenario-list-item tooltip">
