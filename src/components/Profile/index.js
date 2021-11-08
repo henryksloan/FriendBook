8369642794b2535ccc23752fa1906a9b1957461d
Henry Sloan <henryksloan@gmail.com>
2021-11-08 14:33:57 -0500

    Add Profile page and initial styling

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Profile() {
-  // TODO
+import { getFullName, getProfilePic } from '../../utils/profile'
+
+import './index.css';
+import PostList from '../PostList'
+
+function Profile({ match }) {
   return (
     <div className="Profile">
+      <div className="profile-header" style={{ background: "#000" }}>
+        <div className="cover-photo-container">
+          <img className="cover-photo" />
+        </div>
+        <img className="header-profile-pic" src={getProfilePic(match.params.user)} />
+        <h2>{getFullName(match.params.user)}</h2>
+      </div>
+
+      <PostList />
     </div>
   );
+}
+
+Profile.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      user: PropTypes.string
+    })
+  }),
 }
 
 export default Profile;
